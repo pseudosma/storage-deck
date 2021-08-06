33cbb5245714a792b9f614963d22b2f978ab0cb2
pseudosma <pseudosma@users.noreply.github.com>
2021-08-06 04:21:52 +0000

    Update CHANGELOG

--- lib/genericFuncs.d.ts
+++ lib/genericFuncs.d.ts
@@ -1,4 +1,4 @@
 import { Storage, StorageKey, StorageKeyValuePair } from "./storageDeck";
 export declare const addToStorageGeneric: (keyValue: StorageKeyValuePair | StorageKeyValuePair[], store: Storage) => void;
-export declare const retrieveFromStorageGeneric: (keys: string | RegExp | import("searchability").Searchable | StorageKey[], store: Storage, isCustomStorage: boolean) => any;
-export declare const removeFromStorageGeneric: (keys: string | RegExp | import("searchability").Searchable | StorageKey[], store: Storage, isCustomStorage: boolean) => void;
+export declare const retrieveFromStorageGeneric: (keys: StorageKey | StorageKey[], store: Storage, isCustomStorage: boolean) => any;
+export declare const removeFromStorageGeneric: (keys: StorageKey | StorageKey[], store: Storage, isCustomStorage: boolean) => void;
